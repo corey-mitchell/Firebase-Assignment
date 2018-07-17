19909936c63f11a7634290ae9fce54a995dee734
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-07-16 20:08:17 -0500

    finished initial html and css also linked firebase

---
+++ assets/Javascript/firebase.js
@@ -0,0 +1,13 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAgI2MGgnlwTdpSWs8ZimvYD5NO3U3nV4k",
+    authDomain: "database-test-6d0e5.firebaseapp.com",
+    databaseURL: "https://database-test-6d0e5.firebaseio.com",
+    projectId: "database-test-6d0e5",
+    storageBucket: "database-test-6d0e5.appspot.com",
+    messagingSenderId: "575282561382"
+  };
+  firebase.initializeApp(config);
+
+
+  
